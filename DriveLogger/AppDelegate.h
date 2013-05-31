a9040b1872dc8df91b82e72ff286f7ef25d4b33f
Ian Sheret <ian.sheret@polymathinsight.co.uk>
2013-05-31 14:06:29 +0100

    Initial checkin.

---
+++ DriveLogger/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  DriveLogger
+//
+//  Created by Ian Sheret on 31/05/2013.
+//  Copyright (c) 2013 Ian Sheret. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ DriveLogger/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DriveLogger
+//
+//  Created by Ian Sheret on 31/05/2013.
+//  Copyright (c) 2013 Ian Sheret. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
